2298faae09ac7603a9df46f305c61f3046667798
mim-dev <luther.stanton@gmail.com>
2023-01-10 10:35:26 -0500

    initial update

---
+++ MathLib/Math.swift
@@ -0,0 +1,29 @@
+//
+//  Math.swift
+//  MathLib
+//
+//  Created by Luther Stanton on 1/10/23.
+//
+
+import Foundation
+
+public class Math {
+    
+    public init() {
+        
+    }
+    
+    public func addition(operandOne: Int, operandTwo: Int) -> Int {
+ 
+        return operandOne + operandTwo
+        
+//        let additionCalculator = Adder()
+//        return additionCalculator.addIntOperands(operandOne: operandOne, operandTwo: operandTwo)
+    }
+    
+    public func subtraction(operandOne: Int, operandTwo: Int) -> Int {
+        return operandOne - operandTwo
+        
+    }
+    
+}
